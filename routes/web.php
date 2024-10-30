07de7612fecf18c5b35a2341d47865b56788f33b
Agate1234 <faizabiyu72@gmail.com>
2024-10-30 23:18:03 +0700

    percobaan

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\TugasController;
 
-Route::get('/', [LandingController::class, 'index']);
+// Route::get('/', [LandingController::class, 'index']);
 
-Route::get('/tugas', [TugasController::class, 'index']);
+// Route::get('/tugas', [TugasController::class, 'index']);
