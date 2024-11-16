ffc8e75a923877ce606b35affa01b17e12dd34b0
Agate1234 <faizabiyu72@gmail.com>
2024-11-17 00:59:07 +0700

    🥶 Api Tugas Kompen

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\APIController;
+use App\Http\Controllers\Api\APItugasController;
 
 Route::post('/login', [APIController::class, 'login']);
 Route::post('/create_data', [APIController::class, 'postregister']);
 Route::get('/levels', [APIController::class, 'getLevels']);
+Route::post('/logout', [APIController::class, 'logout']);
+
+Route::post('/tugas', [APITugasController::class, 'index']);
+Route::post('/tugas/create_data', [APITugasController::class, 'store']);
+Route::post('/tugas/detail_data', [APITugasController::class, 'show']);
+Route::post('/tugas/update_data', [APITugasController::class, 'edit']);
+Route::post('/tugas/delete_data', [APITugasController::class, 'destroy']);
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +26,6 @@ Route::get('/levels', [APIController::class, 'getLevels']);
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
