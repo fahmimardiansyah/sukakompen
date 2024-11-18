31bed1e3bd707fcb70faffe5caa3e0be5d766c19
Agate1234 <faizabiyu72@gmail.com>
2024-11-18 22:59:20 +0700

    🥶 Tugas Kompen dosen

--- app/Http/Controllers/dosen_tendik/TugasController.php
+++ app/Http/Controllers/dosen_tendik/TugasController.php
@@ -15,24 +15,26 @@ use Illuminate\Http\Request;
 class TugasController extends Controller
 {
     public function index()
-    {
-        $user = auth()->user();
+{
+    $user = auth()->user(); // Mendapatkan user yang sedang login
 
-        $breadcrumb = (object) [
-            'title' => 'Tugas Page',
-            'list' => ['Home', 'Tugas']
-        ];
+    $breadcrumb = (object) [
+        'title' => 'Tugas Page',
+        'list' => ['Home', 'Tugas']
+    ];
 
-        $activeMenu = 'kompen';
+    $activeMenu = 'kompen';
 
-        $tugas = TugasModel::where('user_id', $user->user_id)->get();
+    // Ambil tugas berdasarkan user yang login
+    $tugas = TugasModel::where('user_id', $user->user_id)->get();
 
-        return view('dosen_tendik.kompen.index', [
-            'breadcrumb' => $breadcrumb,
-            'activeMenu' => $activeMenu,
-            'tugas' => $tugas,
-        ]);
-    }
+    return view('dosen_tendik.kompen.index', [
+        'breadcrumb' => $breadcrumb,
+        'activeMenu' => $activeMenu,
+        'tugas' => $tugas,
+    ]);
+}
+
 
     public function create_ajax()
     {
